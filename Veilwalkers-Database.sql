5537da38e897d1988f48aae4c65c7c3a4419e916
Raksze <raksze@gmail.com>
2023-01-13 04:19:15 -0300

    database

---
+++ Veilwalkers-Database.sql
@@ -0,0 +1,17 @@
+create database veilwalkers;
+
+use veilwalkers;
+
+create table email(
+id int not null auto_increment,
+nombre varchar(64),
+email varchar(64),
+primary key (id)
+);
+
+create table beta(
+id int not null auto_increment,
+nombre varchar(64),
+email varchar(64),
+primary key (id)
+);
